509ad3281d9d69091d0c40aeb96cdf91402ca242
malyvsen <5940672+malyvsen@users.noreply.github.com>
2026-02-27 16:46:33 +0100

    feat: install uv with scoop

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -7,5 +7,7 @@ Write-Host "Installing programs..." -ForegroundColor Cyan
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop install git
+scoop bucket add extras
+scoop install uv
 
 Write-Host "Done." -ForegroundColor Green
